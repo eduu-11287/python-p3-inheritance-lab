125ab1c8467caaea64e8b11bb84510d5a3282c52
Edwin <edmboche141@gmail.com>
2025-02-23 11:06:57 +0300

    initial commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,37 @@ from user import User
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        if knowledge is None:
+            knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]   
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        random_element = random.randint(0, len(self.knowledge) -1)
+        return self.knowledge[random_element]
+    
+
+teacher = Teacher("John", "Doe", knowledge)
+print(teacher.teach())  # This should print a random knowledge item
